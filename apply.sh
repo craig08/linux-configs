1a81b86a5bfe4c665b4af596e6a364ce071fef05
Craig Chi <craig10624@gmail.com>
2019-06-21 05:42:44 +0000

    Use sudo to install golang

--- apply.sh
+++ apply.sh
@@ -24,7 +24,7 @@ Unlink() {
 
 InstallGo() {
     wget https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz
-    tar -C /usr/local -xzf go1.12.6.linux-amd64.tar.gz
+    sudo tar -C /usr/local -xzf go1.12.6.linux-amd64.tar.gz
 }
 
 if [ "$1" = "install" ]; then
